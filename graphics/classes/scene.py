93132dc5d5bbcaa4465fda906fabca162cd14fd7
F-Ouail <ouail.f.king@gmail.com>
2024-06-25 21:07:01 +0100

    Update scene.py

--- graphics/classes/scene.py
+++ graphics/classes/scene.py
@@ -19,18 +19,22 @@ class Scene(window.WindowObject):
                 raise TypeError(f"The background Color is not tuple[int, int, int]")
             if not (type(background[0]) == type(background[1]) == type(background[2]) == int):
                 raise TypeError(f"The background Color is not tuple[{type(background[0])}, {type(background[1])}, {type(background[2])}]")
-            self.background = color.Color(background[0], background[1], background[2])
+            self.background = color.Color(background[0],
+                                          background[1],
+                                          background[2])
         elif type(background) is color.Color:
             self.background = background
         else:
-            raise TypeError(f"The background type is one of Color, None,"+
-                f"tuple[int, int, int].The Entred type is {type(background)}")
+            raise TypeError("The background type is one of Color,"
+                            + "None, tuple[int, int, int]."
+                            + f"The Entred type is {type(background)}")
         _window.append(self)
         self.save = ()
     def input_to_update(self, *args) -> None:
         self.save = args
         return
-    def append_most_priority_object(self, object:window.WindowObject) -> None:
+    def append_most_priority_object(self,
+                                    object:window.WindowObject) -> None:
         self.most_priority.append(object)
     def set_should_draw(self, should_draw:bool) -> None:
         if not self.should_draw is should_draw and should_draw:
@@ -39,9 +43,11 @@ class Scene(window.WindowObject):
     def get_should_draw(self) -> bool:
         return self.should_draw
     def append_object(self, obj:window.WindowObject,
-                      xc:bool = False, yc:bool = False) -> None:
+                      centred_horizontally:bool = False,
+                      centred_vertically:bool = False) -> None:
         obj._mouse = window.mouse.pos
-        self.linked_objects.append((obj, xc, yc))
+        self.linked_objects.append((obj, centred_horizontally,                
+                                    centred_vertically))
         self.reset()
     def reset(self) -> None:
         for p in self.most_priority:
