fa43664850eef2689960e6c3e5bb7346881b3654
F-Ouail <ouail.f.king@gmail.com>
2024-06-27 23:57:07 +0100

    Update color.py

--- ogl/classes/color.py
+++ ogl/classes/color.py
@@ -5,10 +5,13 @@ class Color:
         self.b = b
     def value(self) -> tuple[int, int, int]:
         return (self.r ,self.g, self.b)
-    def Not(self):
-        r = self.r + 50 % 255
-        g = self.g + 50 % 255
-        b = self.b + 50 % 255
-        return Color(r,g,b)
+    def opposite(self):
+        return Color(
+            (255 - self.r) % 256,
+            (255 - self.g) % 256,
+            (255 - self.b) % 256
+        )
+    def shift(self):
+        return Color(self.r * 0.85, self.g * 0.85, self.b * 0.85)
     def __str__(self) -> str:
         return f"{self.r} {self.b} {self.g}"
