7bd4d151dd5705e5bb71836e67fcdbc0aded8c80
F-Ouail <ouail.f.king@gmail.com>
2025-06-15 16:06:40 +0100

    Update exemple1.py

--- exemple1.py
+++ exemple1.py
@@ -4,34 +4,22 @@ import ogl
     A simple window with one scene
 """
 
-def on_click(window:ogl.window.Window, button:ogl.ui.Button, *args):
+
+def on_click(window: ogl.window.Window, button: ogl.ui.Button, *args):
     if button.is_mouse_on():
         window.destroy()
+
 
 def main() -> int:
     # Create Objects
     ## Create the Window
-    window = ogl.window.Window(
-        title = "exemple 1",
-        auto_draw = True,
-        resizability = True
-    )
+    window = ogl.window.Window(title="exemple 1", auto_draw=True, resizability=True)
     ## Create the ui.Button
-    button = ogl.ui.Button(
-        text = "Exit"
-    )
+    button = ogl.ui.Button(text="Exit")
     ## Create the Scene (a Container)
-    scene = ogl.scene.Scene(
-        window,
-        name = "scene 1",
-        background = (28, 120, 71)
-    )
+    scene = ogl.scene.Scene(window, name="scene 1", background=(28, 120, 71))
     # Link Objects
-    scene.append_object(
-        button,
-        centred_horizontally = True,
-        centred_vertically = True
-    )
+    scene.append_object(button, centred_horizontally=True, centred_vertically=True)
     # Setup Objects
     window.mouse_handler = lambda *args: on_click(window, button, *args)
     scene.set_should_draw(True)
@@ -39,8 +27,8 @@ def main() -> int:
     while window.running:
         window.show()
     return 0
+
+
 # The Entry Point
 if __name__ == "__main__":
-    exit(
-        main()
-    )
+    exit(main())
